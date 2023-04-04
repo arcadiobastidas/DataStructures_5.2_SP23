797a04767ff10503182a3985507946a3952f906f
arcadiobastidas <abastidas31@gmail.com>
2023-04-03 20:36:56 -0400

    changed main

--- DataStructures_5.2_SP23/DataStructures_5._2_SP23.cpp
+++ DataStructures_5.2_SP23/DataStructures_5._2_SP23.cpp
@@ -18,38 +18,7 @@ int main() {
     converter.convert(infix);
     cout << "Postfix expression: " << converter.getPostfix() << endl;
     
-    //
-    // cout << "a - b + c" << endl;
-    // converter.convert("a - b + c");
-    // cout << converter.getPostfix() << endl;
-    //
-    // cout << "a - (b / c * d)" << endl;
-    // converter.convert("a - (b / c * d)");
-    // cout << converter.getPostfix() << endl;
-    //
-    // cout << "a / (b * c)" << endl;
-    // converter.convert("a / (b * c)");
-    // cout << converter.getPostfix() << endl;
-    //
-    // cout << "a / b / c - (d + e) * f" << endl;
-    // converter.convert("a / b / c - (d + e) * f");
-    // cout << converter.getPostfix() << endl;
-    //
-    // cout << "(a + b) * c" << endl;
-    // converter.convert("(a + b) * c");
-    // cout << converter.getPostfix() << endl;
-    //
-    // cout << "a * (b / c / d) + e" << endl;
-    // converter.convert("a * (b / c / d) + e");
-    // cout << converter.getPostfix() << endl;
-    //
-    // cout << "a - (b + c )" << endl;
-    // converter.convert("a - (b + c )");
-    // cout << converter.getPostfix() << endl;
-    //
-    // cout << "a - (b + c * d) / e" << endl;
-    // converter.convert("a - (b + c * d) / e");
-    // cout << converter.getPostfix() << endl;
+    
 
     return 0;
 }
